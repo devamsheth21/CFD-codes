ba37ffc3782e04278ce8bd686aa2877c812df7de
devamsheth21 <84726617+devamsheth21@users.noreply.github.com>
2021-11-25 02:23:39 +0530

    Add files via upload

---
+++ cfd1.cpp
@@ -0,0 +1,224 @@
+// DEVAM SHETH 18BME109
+//HEADER FILES
+#include<stdio.h>
+#include<math.h>
+#include<fstream>
+#include<dir.h>
+#include<sstream>
+
+using namespace std;
+
+//DEFINITION OF CONSTANT
+#define NI 102
+#define NJ 102
+#define pi 3.141592
+#define t_max 5.5
+#define L 1
+#define rho  1000
+#define Cp 1
+#define H 1
+#define alpha 0.1
+#define DELT 0.0001
+#define q_gen 0
+// USER-DEFINED FUNCTION PROTOTYPE
+void grid_generation();
+void initial_condition();
+void boundary_condition();
+void coefficient();
+void solve_explicit();
+void update();
+void write();
+//GLOBAL VARIABLES
+double x0[NJ][NI],x1[NJ][NI],x2[NJ][NI],x3[NJ][NI];
+double y_0[NJ][NI],y_1[NJ][NI],y2[NJ][NI],y3[NJ][NI];
+
+double XCELL_X[NJ][NI], XCELL_Y[NJ][NI];
+int NCELLI = NI - 1, NCELLJ = NJ - 1;
+
+double s0[NJ][NI], s1[NJ][NI], s2[NJ][NI], s3[NJ][NI];
+double DXF0[NJ][NI], DXF1[NJ][NI], DXF2[NJ][NI], DXF3[NJ][NI];
+double VOL[NJ][NI];
+
+double PHI[NJ][NI], PHI_OLD[NJ][NI];
+double aw[NJ][NI], as[NJ][NI], ae[NJ][NI], an[NJ][NI], ap[NJ][NI];
+int NSTEP=1;
+int i, j;
+
+int main()
+{
+    mkdir("RUN");
+	int step=100;
+    grid_generation();
+    initial_condition();
+    coefficient();
+//do
+for(NSTEP=1;NSTEP<=(t_max/DELT);NSTEP++)
+{
+ //NSTEP=NSTEP + 1;
+ boundary_condition();
+ solve_explicit();
+ update();
+ if( NSTEP% step == 0)
+    {
+
+          write();
+    }
+
+ }
+//while(NSTEP<=t_max);
+}
+void grid_generation()
+{
+// Constant del_x and del_y
+double DELX = L / (NI - 2.0),DELY = H / (NJ - 2.0);
+
+// Co-ordinates of interior nodes
+for (j = 1;j < NCELLJ;j++)
+{
+for (i = 1;i < NCELLI;i++)
+ {
+    x0[j][i] = DELX * (i - 1);
+    x1[j][i] = DELX * i;
+    x2[j][i] = DELX * i;
+    x3[j][i] = DELX * (i - 1);
+    y_0[j][i] = DELX * (j - 1);
+    y_1[j][i] = DELX * (j - 1);
+    y2[j][i] = DELX * j;
+    y3[j][i] = DELX * j;
+    XCELL_X[j][i] = 0.25 * (x0[j][i] + x1[j][i] + x2[j][i] + x3[j][i]);
+    XCELL_Y[j][i] = 0.25 * (y_0[j][i] + y_1[j][i] + y2[j][i] + y3[j][i]);
+ }
+}
+// Co-ordinates of Boundary nodes : W S E N (anti-clockwise)
+for (j = 1;j < NCELLJ;j++)
+  {
+        // West wall
+    XCELL_X[j][0] = 0;
+    XCELL_Y[j][0] = XCELL_Y[j][1]; // same as centroids next to west wall
+    // East wall
+    XCELL_X[j][NCELLI] = L;
+    XCELL_Y[j][NCELLI] = XCELL_Y[j][NCELLI-1]; // same as centroids previous to east wall
+  }
+for (i = 1;i < NCELLI;i++)
+{
+    // South wall
+    XCELL_X[0][i] = XCELL_X[1][i];
+    XCELL_Y[0][i] = 0; // same as centroids above to south wall
+    // North wall
+    XCELL_X[NCELLJ][i] = XCELL_X[NCELLJ-1][i];
+    XCELL_Y[NCELLJ][i] = H; // same as centroids below to north wall
+
+    //corners
+    XCELL_X[NCELLJ][NCELLI]=L;
+    XCELL_Y[NCELLJ][NCELLI]=H;
+    XCELL_X[0][NCELLI]=L;
+    XCELL_X[NCELLJ][0]=0;
+}
+
+// surface area & nodal distance
+ for (j = 1;j < NCELLJ;j++)
+  {
+    for (i = 1;i < NCELLI;i++)
+    {
+     s0[j][i] = -DELY;
+     s1[j][i] = -DELX;
+     s2[j][i] = DELY;
+     s3[j][i] = DELX;
+     DXF0[j][i] = DELX;
+     DXF1[j][i] = DELY;
+     DXF2[j][i] = DELX;
+     DXF3[j][i] = DELY;
+     VOL[j][i] = DELX * DELY;
+    }
+  }
+}
+void initial_condition()
+{
+for (j = 0;j <= NCELLJ;j++)
+{
+for (i = 0;i <= NCELLI;i++)
+{
+PHI_OLD[j][i] = 100.0;
+PHI[j][i] = 100.0;
+}
+}
+}
+void boundary_condition()
+{
+for (j = 0;j <=NCELLJ;j++)
+{
+// West wall
+PHI[j][0] = 300.0; // dirichlet boundary condition
+PHI_OLD[j][0] = 300.0;
+// East wall
+PHI[j][NCELLI] = 100.0; // dirichlet boundary condition
+PHI_OLD[j][NCELLI] = 100.0;
+}
+for (i = 0;i <=NCELLI;i++)
+{
+// South wall
+PHI[0][i] = 200.0; // dirichlet boundary condition
+PHI_OLD[0][i] = 200.0;
+
+
+// North wall
+PHI[NCELLJ][i] = 150.0;// dirichlet boundary condition
+PHI_OLD[NCELLJ][i] = 150.0;
+}
+}
+void coefficient()
+{
+for (j = 1;j < NCELLJ;j++)
+{
+for (i = 1;i < NCELLI;i++)
+{
+aw[j][i] = fabs((alpha*s0[j][i])/DXF0[j][i]);
+as[j][i] = fabs((alpha*s1[j][i])/DXF1[j][i]);
+ae[j][i] = fabs((alpha*s2[j][i])/DXF2[j][i]);
+an[j][i] = fabs((alpha*s3[j][i])/DXF3[j][i]);
+ap[j][i] = aw[j][i] + as[j][i] + ae[j][i] + an[j][i];
+
+}
+}
+}
+void solve_explicit()
+{
+//interior node
+ for (j = 1;j < NCELLJ;j++)
+  {
+    for (i = 1;i < NCELLI;i++)
+    {
+     PHI[j][i] = PHI_OLD[j][i] + (q_gen*DELT)/(rho*Cp)+ ((DELT / VOL[j][i])*( (aw[j][i]*PHI_OLD[j][i- 1]) + (as[j][i]*PHI_OLD[j-1][i]) + (ae[j][i]*PHI_OLD[j][i + 1]) + (an[j][i]*PHI_OLD[j+1][i]) -(ap[j][i]*PHI_OLD[j][i]) ));
+    }
+  }
+}
+void update()
+{
+// interior node
+ for (j = 1;j < NCELLJ;j++)
+  {
+    for (i = 1;i < NCELLI;i++)
+    {
+        PHI_OLD[j][i] = PHI[j][i];
+    }
+  }
+}
+
+void write()
+{
+	ofstream file;
+	stringstream ss;
+	ss << "RUN/TEMP_PROFILE_TIME_" << NSTEP*DELT << ".dat";
+	file.open(ss.str().c_str());
+	file<<"VARIABLES = "<<'"'<<"X"<<'"'<<", "<<'"'<<"Y"<<'"'<<", "<<'"'<<"TEMPERATURE"<<'"'<<endl;
+	file<<"ZONE I="<<NI<<", J="<<NJ<<", F=POINT"<< endl;
+	file<<"SOLUTIONTIME="<<NSTEP*DELT<< endl;
+	for(j=0;j<=NCELLJ;j++)
+	{
+		for(i=0;i<=NCELLI;i++)
+		{
+			file<<XCELL_X[j][i]<<" "<<XCELL_Y[j][i]<<" "<<PHI[j][i]<<endl;
+		}
+	}
+}
+
